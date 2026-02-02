32c90e24e8c3e0201a6486f81c51e0959137d049
Djenabou0506 <baldejenab14@gmail.com>
2026-02-02 15:02:27 +0000

    Ajout de la page Mes Projets avec liste de projets en dur

--- core/views.py
+++ core/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+def projects(request):
+    projets = [
+        {'id': 1, 'titre': 'Blog Django', 'description': 'Blog avec posts et catégories, authentification utilisateurs.'},
+        {'id': 2, 'titre': 'Gestion Station-Service', 'description': 'Application React pour gérer carburant, camions et approvisionnement.'},
+        {'id': 3, 'titre': 'Calculatrice Python', 'description': 'Calculatrice Python avec Git et fonctionnalités avancées.'},
+    ]
+    return render(request, 'core/projects.html', {'projets': projets})
